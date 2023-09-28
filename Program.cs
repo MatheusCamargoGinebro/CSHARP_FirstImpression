77e84b566c51e6d25173ec8aafadf259d28dadcb
MatheusCamargoGinebro <matheus.ginebro@aluno.ifsp.edu.br>
2023-09-28 09:01:55 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Usando biblioteca base de entrada e saídas do sistema:
+using System; // Import da biblioteca "#include <stdin.h>"
+
+// Iniciando o programa:
+class principal
+{
+    // Método principal do programa: 
+    static void Main()
+    {
+        Console.Write("Olá mundo!");
+
+
+    }
+}
